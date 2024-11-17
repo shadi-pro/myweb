492b567b14127fae1dd47ca4d6d14d1518bb3fdd
shandowpro <shadisayed.6891@gmail.com>
2024-11-17 15:22:58 +0200

    new push

---
+++ Dave Gray/Node Js Dave Gray course/Course Projects/2 Nodejs  Filies controling system/math.js
@@ -0,0 +1,23 @@
+// New testing file in  [node core Module ]      :
+// Inluding some defined testing  functions [ with  exoporting  by (2)  metdos ] :
+
+// Default defintions  :    
+// const add = (a, b) =>   a + b
+// const sub = (a, b) => a - b
+// const mult = (a, b) =>  a * b 
+
+//  Exportive definition   :
+exports.add = (a, b) =>   a + b
+exports.sub = (a, b) => a - b
+exports.mult = (a, b) =>  a * b 
+exports.divid = (a, b) => a / b
+
+// Exportng all  the defined functions 
+// module.exports = { add , sub , mult }
+ 
+
+
+
+
+
+
